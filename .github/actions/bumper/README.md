27b793d0240c2190a447002e43e679e4d9b53004
Chip Pate <cpate4@gmail.com>
2025-03-24 11:37:13 -0400

    initial commit

---
+++ .github/actions/bumper/README.md
@@ -0,0 +1,136 @@
+
+# bumper
+
+This is a fork of [github-tag-action](https://github.com/anothrNick/github-tag-action)
+It has been extended for Arboretum workflow
+
+A Github Action to automate version bumping.
+
+## Usage
+
+```Dockerfile
+name: Bump version
+on:
+  push:
+    branches:
+    - main
+    - 'hotfix**'
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v3
+      with:
+        token: ${{ secrets.GITHUB_TOKEN }}
+    - name: Bump version and push tag
+      uses: broadinstitute/github-actions/actions/bumper
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        WITH_V: true
+        RELEASE_BRANCHES: main
+        DEFAULT_BUMP: patch
+        HOTFIX_BRANCHES: hotfix.*
+        VERSION_FILE_PATH: build.gradle
+        VERSION_LINE_MATCH: "^version \'.*\'"
+```
+
+### Options
+
+#### Environment Variables
+
+* **GITHUB_TOKEN** ***(required)*** - Required for permission to tag the repo.
+* **DEFAULT_BUMP** *(optional)* -
+  Which type of bump to use when none explicitly provided (default: `minor`).
+* **OVERRIDE_BUMP** *(optional)* -
+  Overrides the default bump and any commit message bump indicator
+  for which part of the semver to bump
+* **WITH_V** *(optional)* - Tag version with `v` character.
+* **RELEASE_BRANCHES** *(optional)* -
+  Comma separated list of branches (bash reg exp accepted) that will generate
+  the release tags. Other branches and pull-requests generate versions
+  postfixed with the commit hash and do not generate any tag.
+  Examples: `master` or `.*` or `release.*,hotfix.*,master` ...
+* **HOTFIX_BRANCHES** *(optional)* - Comma separated list of branches (bash reg exp
+  accepted) that will generate hotfix tags. Example: `hotfix.*`
+* **SOURCE** *(optional)* -
+  Operate on a relative path under $GITHUB_WORKSPACE.
+* **DRY_RUN** *(optional)* -
+  Determine the next version without tagging the branch.
+  The workflow can use the outputs `new_tag` and `tag` in subsequent steps.
+  Possible values are ```true``` and ```false``` (default).
+* **INITIAL_VERSION** *(optional)* -
+  Set initial version before bump. Default `0.0.0`.
+* **TAG_CONTEXT** *(optional)* -
+  Set the context of the previous tag.
+  Possible values are `repo` (default) or `branch`.
+* **VERSION_FILE_PATH** *(optional)* -
+  If present, update the version number in that file
+* **VERSION_LINE_MATCH** *(optional)* -
+  If present, a grep regular expression to identify the line containing
+  the version number in the VERSION_FILE_PATH.
+* **VERSION_SUFFIX** *(optional)* -
+  Suffix added to the version in the version file, such as, -SNAPSHOT
+* **FORCE_WITHOUT_CHANGES** *(optional)* -
+  Bump even if there are no changes from the previous version.
+
+#### Outputs
+
+* **new_tag** - The value of the newly created tag.
+* **tag** - The value of the latest tag after running this action.
+* **part** - The part of version which was bumped.
+
+> ***Note:*** This action creates a [lightweight tag](https://developer.github.com/v3/git/refs/#create-a-reference).
+
+### Bumping
+
+The part of the semantic version to bump is controlled in this order:
+
+ 1. If in a hotfix branch (as defined by **HOTFIX_BRANCHES**),
+    bump the value in the hotfix extension.
+    If no extension is previously tagged, use `hotfix.0`.
+ 2. If **OVERRIDE_BUMP** is set, bump that part of the semvar:
+    `major`, `minor`, or `patch`
+ 3. If any commit message includes `#major`, `#minor`, or `#patch`,
+    that will trigger the respective version bump.
+    If two or more are present, the highest-ranking one will take precedence.
+ 4. If **DEFAULT_BUMP** is set, it will bump that part of the semvar: `major`, `minor`,
+    or `patch`. if **DEFAULT_BUMP** is `none`, then no version bump will be performed.
+ 5. The `patch` version is bumped
+
+> ***Note:***
+This action **will not** bump the tag if the `HEAD` commit has already been tagged.
+
+### Workflow
+
+#### Normal Flow
+
+* Develop a code change in a working branch
+* Make a PR from the working branch to `main`
+* If you made changes that would merit a minor or major version bump,
+  then before merging the PR, set the commit message to include `#major` or `#minor`.
+* Merging the PR triggers the action. It will
+  * Get the latest tag
+  * Bump the tag as described above
+  * Write the tag to the specified version file
+  * Push the tag to github
+
+#### Hotfix Flow
+
+* Prepare a hotfix branch:
+  * Checkout the tag you need to hot fix into a branch named `hotfix/<something>`
+  * `git push origin hotfix/<something>` to create the hotfix branch
+  * `git checkout -b fixdevbranch` from the hotfix branch
+  * Make the hot fix, commit, and push `fixdevbranch`
+  * Make a PR to merge `fixdevbranch` into `hotfix/<something>`
+  * When it is ready, merge.
+* Merging the PR triggers the action. It will
+  * Get the latest tag
+  * Either append `-hotfix.0` or increment the existing hotfix number; e.g., `-hotfix.1`
+  * Write the tag to the version file
+  * Push the tag to github
+
+### Credits
+
+[fsaintjacques/semver-tool](https://github.com/fsaintjacques/semver-tool)
+
+[github-tag-action](https://github.com/anothrNick/github-tag-action)
---
+++ .github/actions/bumper/entrypoint.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+# config
+default_semvar_bump=${DEFAULT_BUMP:-patch}
+override_semvar_bump=${OVERRIDE_BUMP}
+with_v=${WITH_V:-false}
+release_branches=${RELEASE_BRANCHES:-main}
+hotfix_branches=${HOTFIX_BRANCHES:-hotfix.*}
+source=${SOURCE:-.}
+dryrun=${DRY_RUN:-false}
+initial_version=${INITIAL_VERSION:-0.0.0}
+tag_context=${TAG_CONTEXT:-repo}
+version_file_path=${VERSION_FILE_PATH}
+version_line_match=${VERSION_LINE_MATCH}
+version_suffix=${VERSION_SUFFIX}
+hotfix_version=${HOTFIX_VERSION}
+force_without_changes=${FORCE_WITHOUT_CHANGES:-false}
+
+cd "${GITHUB_WORKSPACE}"/"${source}" || exit
+git config --global --add safe.directory $(pwd)
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+hotfix_release="false"
+IFS=',' read -ra branch <<<"$hotfix_branches"
+for b in "${branch[@]}"; do
+  echo "Is $b a match for ${current_branch}"
+  if [[ ${current_branch} =~ $b ]]; then
+    hotfix_release="true"
+  fi
+done
+echo "hotfix_release = $hotfix_release"
+
+# By definition, if it is a hotfix release, it is not pre_release
+if $hotfix_release; then
+  pre_release="false"
+else
+  pre_release="true"
+  IFS=',' read -ra branch <<<"$release_branches"
+  # Updated to same logic as upstream github-tag-action
+  # https://github.com/anothrNick/github-tag-action/blob/master/entrypoint.sh#L62-#L74
+  for b in "${branch[@]}"; do
+    # check if ${current_branch} is in ${release_branches} | exact branch match
+    echo "Is $b a match for ${current_branch}"
+    if [[ $current_branch == "$b" ]]; then
+      pre_release="false"
+    fi
+    # verify non specific branch names like  .* release/* if wildcard filter then =~
+    echo "Is $b a non specific branch names like  .* release/*, if so do wildcard filter then =~"
+    if [ "$b" != "${b//[\[\]|.? +*]/}" ] && [[ $current_branch =~ $b ]]; then
+      pre_release="false"
+    fi
+  done
+fi
+echo "pre_release = $pre_release"
+
+# fetch tags
+git fetch --tags
+
+# get latest tag & version that looks like a semver (with or without v, using with_v)
+if $with_v; then
+  tag_pattern="v[0-9]*.[0-9]*.[0-9]*"
+  version_pattern="(.*)v([0-9]+\.[0-9]+\.[0-9]+-?[a-zA-Z0-9]*)(.*)"
+else
+  tag_pattern="[0-9]*.[0-9]*.[0-9]*"
+  version_pattern="(.*)([0-9]+\.[0-9]+\.[0-9]+-?[a-zA-Z0-9]*)(.*)"
+fi
+
+case "$tag_context" in
+*repo*) tag=$(git for-each-ref --sort=-committerdate --count=1 --format '%(refname)' "refs/tags/$tag_pattern" | cut -d / -f 3-) ;;
+*branch*) tag=$(git describe --tags --match "*[v0-9].*[0-9\.]" --abbrev=0) ;;
+*)
+  echo "Unrecognised context"
+  exit 1
+  ;;
+esac
+
+# get current commit hash for tag
+# on the initial use, this shows usage, because $tag is empty. The logic below still works.
+tag_commit=$(git rev-list -n 1 "$tag")
+
+# get current commit hash
+commit=$(git rev-parse HEAD)
+
+if [ "$tag_commit" == "$commit" ] && [ "$force_without_changes" == "false" ]; then
+  echo "No new commits since previous tag. Skipping..."
+  echo tag="$tag" >>"$GITHUB_OUTPUT"
+  exit 0
+fi
+
+# if there are none, start tags at INITIAL_VERSION which defaults to 0.0.0
+if [ -z "$tag" ]; then
+  log=$(git log --pretty='%B')
+  tag="$initial_version"
+else
+  log=$(git log "$tag"..HEAD --pretty='%B')
+fi
+echo "$log"
+
+HOTFIX_REGEX="^hotfix\\.(0|[1-9][0-9]*)$"
+if $hotfix_release; then
+  # For hotfix release, we compute the hotfix string and do not bump any fields in the version number part
+  if [ -n "$hotfix_version" ]; then
+    # Specific hotfix version specified
+    hotfix_string="hotfix.${hotfix_version}"
+  else
+    # Compute the hotfix string by trying to read the version from the prerel
+    # If it isn't there or isn't in our hotfix format, then we use hotfix.0
+    hotfix_string="hotfix.0"
+    prerel=$(semver get prerel "$tag")
+    echo "get prerel is '$prerel'"
+    if [ -n "$prerel" ]; then
+      # We have a prerel. See if it is a proper hotfix
+      if [[ $prerel =~ $HOTFIX_REGEX ]]; then
+        hotfix_incr="$((${BASH_REMATCH[1]} + 1))"
+        hotfix_string="hotfix.$hotfix_incr"
+      else
+        echo "prerel did not match hotfix regex"
+      fi
+    fi
+  fi
+  new=$(semver bump prerel "$hotfix_string" "$tag")
+  part="hotfix"
+else
+  # For non-hotfix, we compute the new version
+  # If there is an override for what part to bump, use that. If there is a #xyz in the log message, use that.
+  # Othewise, use the default part.
+  if [ -n "$override_semvar_bump" ]; then
+    part=$override_semvar_bump
+  else
+    # No override, check the commit message.
+    # Failing that, use the default.
+    case "$log" in
+    *#major*) part="major" ;;
+    *#minor*) part="minor" ;;
+    *#patch*) part="patch" ;;
+    *)
+      if [ "$default_semvar_bump" == "none" ]; then
+        echo "Default bump was set to none. Skipping..."
+        exit 0
+      else
+        part="${default_semvar_bump}"
+      fi
+      ;;
+    esac
+  fi
+  new=$(semver bump "$part" "$tag")
+fi
+
+echo "Updated semver part $part"
+
+# did we get a new tag?
+if [ ! -z "$new" ]; then
+  # prefix with 'v'
+  if $with_v; then
+    new="v$new"
+  fi
+
+  if $pre_release; then
+    new="$new-${commit:0:7}"
+  fi
+fi
+echo "New tag is $new"
+
+# set outputs
+echo new_tag="$new" >>"$GITHUB_OUTPUT"
+echo part="$part" >>"$GITHUB_OUTPUT"
+
+# use dry run to determine the next tag
+if $dryrun; then
+  echo tag="$tag" >>"$GITHUB_OUTPUT"
+  exit 0
+fi
+
+echo tag="$new" >>"$GITHUB_OUTPUT"
+
+if $pre_release; then
+  echo "This branch is not a release branch. Skipping the tag creation."
+  exit 0
+fi
+
+# bump the version in the file located at ${VERSION_FILE_PATH}
+# expects that there is a line that matches the regular expression: ${VERSION_LINE_MATCH}
+# we replaced that line with one that has the updated version, possibly including the version suffix
+if [ -z "$version_file_path" ] || [ -z "$version_line_match" ]; then
+  echo "Skipping bump of version file."
+else
+  if [ -z "$version_suffix" ]; then
+    version_new=$new
+  else
+    version_new=${new}-${version_suffix}
+  fi
+
+  version_line=$(cat "$version_file_path" | grep -e "${version_line_match}")
+  if [ -z "$version_line" ]; then
+    echo "No version line found; no bump of version file."
+  else
+    new_line=$(echo "$version_line" | sed -E -e "s/$version_pattern/\1${version_new}\3/")
+    sed -E -i.bak -e "s/${version_line}/${new_line}/" "$version_file_path"
+  fi
+  git config --global user.email "robot@terra.team"
+  git config --global user.name "bumptagbot"
+  git add "$version_file_path"
+  git commit -m "bump ${new}"
+  git push origin "$current_branch"
+  commit=$(git rev-parse HEAD)
+fi
+
+# push new tag ref to github
+dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
+full_name=$GITHUB_REPOSITORY
+git_refs_url=$(jq .repository.git_refs_url "$GITHUB_EVENT_PATH" | tr -d '"' | sed 's/{\/sha}//g')
+
+echo "$dt: **pushing tag $new to repo $full_name"
+
+curl -s -X POST "$git_refs_url" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -d @- <<EOF
+
+{
+  "ref": "refs/tags/$new",
+  "sha": "$commit"
+}
+EOF
